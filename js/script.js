d0a31af22634f97a0af86ca3e5aa5ac8db3dd8b4
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-07-24 15:13:07 +0530

    Create function to return random choices
    + Used the `Math.floor()` and `Math.random()` functions to return random numbers between 1 to 3.
    + Assigned '1' to "rock" and '2' to "paper" and '3' to "scissors".
    
    Whenever the function is being called, it will return either "rock", "paper" or "scissors".

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+
+  if (randomNumber === 1) {
+    return "rock";
+  } else if (randomNumber === 2) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
